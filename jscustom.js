06dd1575a9d6ce1c645faebe8c2cf349a863feb3
丨浅笑安然丨 <49086599+L-20021213@users.noreply.github.com>
2021-09-17 13:23:22 +0800

    Update jscustom.js

--- jscustom.js
+++ jscustom.js
@@ -1,1 +1,26 @@
-
+// 禁止调试
+(function noDebuger() {
+    function testDebuger() {
+        var d = new Date();
+        debugger;
+        if (new Date() - d > 10) {
+            document.body.innerHTML = '<div style="width: 100%;height: 50px;font-size: 30px;text-align: center;font-weight: bold;">啊这个网站炸了，<a href="/" target="_blank" style="color:#4285f4;">点击返回</a>试试吧~</div>';
+            return true;
+        }
+        return false;
+    }
+    function start() {
+        while (testDebuger()) {
+            testDebuger();
+        }
+    }
+    if (!testDebuger()) {
+        window.onblur = function () {
+            setTimeout(function () {
+                start();
+            }, 500)
+        }
+    }else {
+        start();
+    }
+})();
